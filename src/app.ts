c6b57b6f4b0a61f6837b402b8dcb9b209ca7bc1c
FGVargas <vargasmarquezjosemanuel@gmail.com>
2024-11-05 13:35:37 -0600

    example

--- src/app.ts
+++ src/app.ts
@@ -2,55 +2,24 @@ import { createBot } from '@builderbot/bot'
 import { adapterProvider } from './provider'
 import { adapterDB } from './database'
 import { adapterFlow } from './flow'
-
-const PORT = process.env.PORT ?? 3008
+import dotenv from 'dotenv';
+import * as process from "process";
+dotenv.config();
+const PORT =  3008
 
 
 const main = async () => {
-    
-
     const { handleCtx, httpServer } = await createBot({
         flow: adapterFlow,
         provider: adapterProvider,
         database: adapterDB,
     })
-
     adapterProvider.server.post(
         '/v1/messages',
         handleCtx(async (bot, req, res) => {
             const { number, message, urlMedia } = req.body
             await bot.sendMessage(number, message, { media: urlMedia ?? null })
             return res.end('sended')
-        })
-    )
-
-    adapterProvider.server.post(
-        '/v1/register',
-        handleCtx(async (bot, req, res) => {
-            const { number, name } = req.body
-            await bot.dispatch('REGISTER_FLOW', { from: number, name })
-            return res.end('trigger')
-        })
-    )
-
-    adapterProvider.server.post(
-        '/v1/samples',
-        handleCtx(async (bot, req, res) => {
-            const { number, name } = req.body
-            await bot.dispatch('SAMPLES', { from: number, name })
-            return res.end('trigger')
-        })
-    )
-
-    adapterProvider.server.post(
-        '/v1/blacklist',
-        handleCtx(async (bot, req, res) => {
-            const { number, intent } = req.body
-            if (intent === 'remove') bot.blacklist.remove(number)
-            if (intent === 'add') bot.blacklist.add(number)
-
-            res.writeHead(200, { 'Content-Type': 'application/json' })
-            return res.end(JSON.stringify({ status: 'ok', number, intent }))
         })
     )
 
